a2e699e6f0426fd9d4535c6135244b3b45844aee
williamsalas <williamsalas24@gmail.com>
2021-03-05 22:28:30 -0800

    implemented new button functions and layout

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +17,10 @@ import android.widget.TextView;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /* Pokeapi imports */
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
@@ -24,7 +31,7 @@ import me.sargunvohra.lib.pokekotlin.model.PokemonSprites;
 
 public class MainActivity extends AppCompatActivity {
     PokeApi pokeApi = new PokeApiClient();
-
+    final PokeObject[] po = {null, null};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +39,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @SuppressLint("SetTextI18n")
+    public void onPokemon1AttackButtonClick(View v) {
+        if (po[0] != null && po[1] != null) {
+            po[1].health.takeDamage((int) (Math.random() * 50));
+
+            TextView pokeHP2 = (TextView) findViewById(R.id.pokemon_2_HP);
+            pokeHP2.setText(po[1].health.getCurrentHP() + " HP");
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onPokemon2AttackButtonClick(View v) {
+        if (po[0] != null && po[1] != null) {
+            po[0].health.takeDamage((int) (Math.random() * 50));
+
+            TextView pokeHP1 = (TextView) findViewById(R.id.pokemon_1_HP);
+            pokeHP1.setText(po[0].health.getCurrentHP() + " HP");
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onPokemon1HealButtonClick(View v) {
+        if (po[0] != null && po[1] != null) {
+            po[0].health.healDamage((int) (Math.random() * 50));
+
+            TextView pokeHP1 = (TextView) findViewById(R.id.pokemon_1_HP);
+            pokeHP1.setText(po[0].health.getCurrentHP() + " HP");
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onPokemon2HealButtonClick(View v) {
+        if (po[0] != null && po[1] != null) {
+            po[1].health.healDamage((int) (Math.random() * 30));
+
+            TextView pokeHP2 = (TextView) findViewById(R.id.pokemon_2_HP);
+            pokeHP2.setText(po[1].health.getCurrentHP() + " HP");
+        }
+    }
+
+
     // the @SuppressLint allows string literals in the onButtonClick function
     @SuppressLint("SetTextI18n")
-    public void onButtonClick(View v) {
-
+    public void onGenerateButtonClick(View v) {
         Random rand = new Random();
-        int myPokeNum = rand.nextInt(151) + 1; // get a random number between 1 and 151 inclusive
-        boolean isShiny = false;
-        if (rand.nextDouble() <= 0.25)
-            isShiny = true;
+        // final Integer[] myPokeNums = {null, null};
+//        final int myPokeNum1 = rand.nextInt(151) + 1; // get a random number between 1 and 151 inclusive
+//        final int myPokeNum2 = rand.nextInt(151) + 1; // get a random number between 1 and 151 inclusive
+        //final boolean []isShiny = {rand.nextDouble() <= 0.25, rand.nextDouble() <= 0.25} ;
 
-        final Pokemon[] myPoke = {null};
-        final PokemonSpecies[] myPokeSpecies = {null};
-        final Bitmap[] bitmap = {null};
 
-        boolean finalShiny = isShiny; // had to use for some reason
-        Thread thread = new Thread() {
-
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    // grab reference to the Pokemon, PokemonSpecies, and Sprites via api call
-                    // api calls must happen inside threads
-                    myPoke[0] = pokeApi.getPokemon(myPokeNum);
-                    myPokeSpecies[0] = pokeApi.getPokemonSpecies(myPokeNum);
-                    PokemonSprites myPokeSprites = myPoke[0].getSprites();
-                    String frontSprite;
-                    if (finalShiny)
-                        frontSprite = myPokeSprites.getFrontShiny();
-                    else
-                        frontSprite = myPokeSprites.getFrontDefault();
-                    bitmap[0] = BitmapFactory.decodeStream((InputStream) new URL(frontSprite).getContent()); // networking
-                } // end run
-                catch (Exception e) {
-                    e.printStackTrace();
-                } // end catch
+                Looper.prepare();
+                for (int i = 0; i < 2; i++) {
+                    po[i] = new PokeObject(rand.nextInt(151) + 1);
+                }
+                ;
             }
-        };
+        });
+
 
         thread.start(); // start the thread
         // wait for thread to finish before continuing
@@ -83,21 +115,25 @@ public class MainActivity extends AppCompatActivity {
          */
 
         // grab reference to relevant data fields
-        TextView tv1 = (TextView) findViewById(R.id.textView);
-        TextView tv2 = (TextView) findViewById(R.id.textView2);
-        TextView tv3 = (TextView) findViewById(R.id.textView3);
-        ImageView i = (ImageView) findViewById(R.id.logoImageView);
+        TextView pokeName1 = (TextView) findViewById(R.id.pokemon_1_name);
+        TextView pokeName2 = (TextView) findViewById(R.id.pokemon_2_name);
 
-        // set the appropriate sprite
-        i.setImageBitmap(bitmap[0]);
+        pokeName1.setText(po[0].getName());
+        pokeName2.setText(po[1].getName());
 
-        // set the text fields appropriately with API calls
-        tv1.setText("Num: " + myPokeSpecies[0].getId());
-        if (finalShiny)
-            tv2.setText("shiny " + myPokeSpecies[0].getName());
-        else
-            tv2.setText(myPokeSpecies[0].getName());
-        tv3.setText("Capture rate: " + myPokeSpecies[0].getCaptureRate());
+        ImageView image1 = (ImageView) findViewById(R.id.pokemon_1_sprite);
+        ImageView image2 = (ImageView) findViewById(R.id.pokemon_2_sprite);
+
+        // set the image accordingly
+        image1.setImageBitmap(po[0].getBitmap());
+        image2.setImageBitmap(po[1].getBitmap());
+
+        TextView pokeHP1 = (TextView) findViewById(R.id.pokemon_1_HP);
+        TextView pokeHP2 = (TextView) findViewById(R.id.pokemon_2_HP);
+
+        pokeHP1.setText(po[0].health.getCurrentHP() + " HP");
+        pokeHP2.setText(po[1].health.getCurrentHP() + " HP");
+
 
     } // end onButtonClick
 }
