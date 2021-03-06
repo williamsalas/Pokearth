f1656bf53e5a4f6f6bad1681cf2c289ad1e70508
Alex Lux <alexlux@live.com>
2021-03-06 13:11:20 -0800

    created the play button functionality that takes the user to pokemon select(play page)

--- app/src/main/java/com/example/pokearth/MainActivity.java
+++ app/src/main/java/com/example/pokearth/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.pokearth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        button = findViewById(R.id.play_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayActivity();
+            }
+        });
+    }
+
+    public void openPlayActivity() {
+        Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+        startActivity(intent);
     }
 }
