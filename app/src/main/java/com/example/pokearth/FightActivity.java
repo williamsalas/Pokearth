a09381a738883ab86dca12b1d452056ff54c79e0
williamsalas <williamsalas24@gmail.com>
2021-03-29 22:20:58 -0700

    Implemented move selector and enemy turn logic

--- app/src/main/java/com/example/pokearth/FightActivity.java
+++ app/src/main/java/com/example/pokearth/FightActivity.java
@@ -3,13 +3,12 @@ package com.example.pokearth;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -21,73 +20,11 @@ public class FightActivity extends AppCompatActivity {
 
 
     final PokeObject[] po = {null, null};
-    TextView textView, textView2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fight_page);
-
-
-        textView = findViewById(R.id.playerPokemonNameTextView);
-        ColorDrawable leftBorder1 = new ColorDrawable(Color.BLACK);
-        ColorDrawable topBorder1 = new ColorDrawable(Color.BLACK);
-        ColorDrawable rightBorder1 = new ColorDrawable(Color.WHITE);
-        ColorDrawable bottomBorder1 = new ColorDrawable(Color.WHITE);
-        ColorDrawable background1 = new ColorDrawable(Color.WHITE);
-
-        Drawable[] layers1 = new Drawable[]{
-                leftBorder1,
-                topBorder1,
-                rightBorder1,
-                bottomBorder1,
-                background1
-        };
-
-        LayerDrawable layerDrawable1 = new LayerDrawable(layers1);
-
-        // Left Border
-        layerDrawable1.setLayerInset(0, 0, 0, 15, 0);
-        // Top Border
-        layerDrawable1.setLayerInset(1, 15, 0, 15, 0);
-        // Right Border
-        layerDrawable1.setLayerInset(2, 15, 15, 0, 0);
-        // Bottom Border
-        layerDrawable1.setLayerInset(3, 15, 15, 15, 0);
-        // Background
-        layerDrawable1.setLayerInset(4, 15, 15, 15, 15);
-
-        textView.setBackground(layerDrawable1);
-
-        textView2 = findViewById(R.id.opponentPokemonNameTextView);
-        ColorDrawable leftBorder2 = new ColorDrawable(Color.WHITE);
-        ColorDrawable topBorder2 = new ColorDrawable(Color.WHITE);
-        ColorDrawable rightBorder2 = new ColorDrawable(Color.BLACK);
-        ColorDrawable bottomBorder2 = new ColorDrawable(Color.BLACK);
-        ColorDrawable background2 = new ColorDrawable(Color.WHITE);
-
-        Drawable[] layers2 = new Drawable[]{
-                leftBorder2,
-                topBorder2,
-                rightBorder2,
-                bottomBorder2,
-                background2
-        };
-
-        LayerDrawable layerDrawable2 = new LayerDrawable(layers2);
-
-        // Left Border
-        layerDrawable2.setLayerInset(0, 0, 0, 15, 0);
-        // Top Border
-        layerDrawable2.setLayerInset(1, 15, 0, 15, 0);
-        // Right Border
-        layerDrawable2.setLayerInset(2, 15, 15, 0, 0);
-        // Bottom Border
-        layerDrawable2.setLayerInset(3, 15, 15, 15, 0);
-        // Background
-        layerDrawable2.setLayerInset(4, 15, 15, 15, 15);
-
-        textView2.setBackground(layerDrawable2);
 
         GenerateRunnable runnable = new GenerateRunnable();
         new Thread(runnable).start();
@@ -98,6 +35,58 @@ public class FightActivity extends AppCompatActivity {
     public void openPlayActivity(View v) {
         Intent intent = new Intent(FightActivity.this, PlayActivity.class);
         startActivity(intent);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void playerAttackButtonClick(View v) {
+        if (po[0] != null && po[1] != null) {
+
+            TextView battleText = (TextView) findViewById(R.id.battleTextView);
+            LinearLayout fightButtonsLinearLayout = (LinearLayout) findViewById(R.id.fightButtonsLinearLayout);
+
+            fightButtonsLinearLayout.setVisibility(View.INVISIBLE);
+            battleText.setVisibility(View.VISIBLE);
+
+            if (po[0].health.getCurrentHP() != 0 && po[1].health.getCurrentHP() != 0) {
+
+                po[0].attack(po[1], battleText);
+                TextView opponentPokemonHPTextView = (TextView) findViewById(R.id.opponentPokemonHPTextView);
+                opponentPokemonHPTextView.setText(po[1].health.getCurrentHP() + " HP");
+
+                Runnable opponentMove = new Runnable() {
+                    @Override
+                    public void run() {
+                        po[1].attack(po[0], battleText);
+                        TextView playerPokemonHPTextView = (TextView) findViewById(R.id.playerPokemonHPTextView);
+                        playerPokemonHPTextView.setText(po[0].health.getCurrentHP() + " HP");
+                    }
+                };
+
+
+                if (po[1].health.getCurrentHP() != 0) {
+                    Handler h = new Handler();
+                    h.postDelayed(opponentMove, 3000); // simulate enemy thinking
+                }
+
+            } else {
+                battleText.setText("You can't do that! " + po[1].getName() + " is already fainted!");
+            }
+        }
+    }
+
+    public void displayAttackMenu(View v) {
+        // if toggled off, toggle the ui that has attack1, attack2
+        // if toggled on, turn off
+        LinearLayout fightButtonsLinearLayout = (LinearLayout) findViewById(R.id.fightButtonsLinearLayout);
+        TextView battleTextView = (TextView) findViewById(R.id.battleTextView);
+
+        if (fightButtonsLinearLayout.getVisibility() == View.INVISIBLE) {
+            fightButtonsLinearLayout.setVisibility(View.VISIBLE);
+            battleTextView.setVisibility(View.INVISIBLE);
+        } else {
+            fightButtonsLinearLayout.setVisibility(View.INVISIBLE);
+            battleTextView.setVisibility(View.VISIBLE);
+        }
     }
 
     class GenerateRunnable implements Runnable {
@@ -140,11 +129,17 @@ public class FightActivity extends AppCompatActivity {
                     TextView battleText = (TextView) findViewById(R.id.battleTextView);
                     battleText.setText("A wild " + po[1].getName() + " appeared!");
 
-                    View pokeAttackButton1 = findViewById(R.id.fightButton);
-                    //View pokeAttackButton2 = findViewById(R.id.button2);
+                    View pokeAttackButton1 = findViewById(R.id.move1Button);
+                    View pokeAttackButton2 = findViewById(R.id.move2Button);
 
                     pokeAttackButton1.setBackgroundColor(Color.parseColor(po[0].getTypeColorString(0)));
-                    // pokeAttackButton2.setBackgroundColor(Color.parseColor(po[1].getTypeColorString(0)));
+
+                    if (po[0].isDualType()) {
+                        pokeAttackButton2.setBackgroundColor(Color.parseColor(po[0].getTypeColorString(1)));
+
+                    } else {
+                        pokeAttackButton2.setVisibility(View.INVISIBLE);
+                    }
 
 
                 } // end run
@@ -153,6 +148,6 @@ public class FightActivity extends AppCompatActivity {
             }); // end run
 
         }
-    }
+    } // end GenerateRunnable
 
 } // end fight activity
