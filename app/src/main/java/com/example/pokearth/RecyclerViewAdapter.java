e73f8b61564548440c70a42c87249ac52c3e9492
williamsalas <williamsalas24@gmail.com>
2021-05-12 15:49:43 -0700

    added pokemon type coloring to names in battle activity

--- app/src/main/java/com/example/pokearth/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/pokearth/RecyclerViewAdapter.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -54,9 +53,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 selectedItemPosition = position;
                 BiomeActivity.setChosenBiome(selectedItemPosition);
                 notifyDataSetChanged();
-
-                // holder.itemView.setBackgroundColor(Color.LTGRAY);
-                Toast.makeText(mContext.getApplicationContext(), mImageNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
 
