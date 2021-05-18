cc26c5032cf76700f74db2ad3fee5b9c90b169b6
Saul-V <saul.villegas278@myci.csuci.edu>
2021-05-18 13:06:02 -0700

    Merge this to main

--- app/src/main/java/com/example/pokearth/FightActivity.java
+++ app/src/main/java/com/example/pokearth/FightActivity.java
@@ -1,6 +1,7 @@
 package com.example.pokearth;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
